f2044423979d72e8ad07c79aef8ce9e83f4de509
mvaughan <magnus.vaughan@gmail.com>
2019-05-05 18:42:27 +0100

    Import character script working

--- characters/management/commands/populate_db.py
+++ characters/management/commands/populate_db.py
@@ -11,6 +11,21 @@ class Command(BaseCommand):
         data = json.loads(data_string)
 
         for person in data:
+
+            if not isinstance(person['Name'], str):
+                person['Name'] = ''
+            if not isinstance(person['Culture'], str):
+                person['Culture'] = ''
+            if not isinstance(person['Born'], str):
+                person['Born'] = ''
+            if not isinstance(person['Died'], str):
+                person['Died'] = ''
+            if not isinstance(person['Father'], str):
+                person['Father'] = ''
+            if not isinstance(person['Mother'], str):
+                person['Mother'] = ''
+            if not isinstance(person['Spouse'], str):
+                person['Spouse'] = ''
     
             from characters.models import Character
             record = Character(id=person['Id'], name=person['Name'], is_female=person['IsFemale'], culture=person['Culture'], titles=person['Titles'], aliases=person['Aliases'], born=person['Born'], died=person['Died'], father=person['Father'], mother=person['Mother'], spouse=person['Spouse'], children=person['Children'], allegiances=person['Allegiances'], books=person['Books'], pov_books=person['PovBooks'], played_by=person['PlayedBy'], tv_series=person['TvSeries'])
