760637ff203d5cfc3fd1272dbc6d3be1131e597a
mvaughan <magnus.vaughan@gmail.com>
2019-05-07 18:49:45 +0100

    Frontend Data bieng returned

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,7 +4,7 @@ import DataProvider from "./DataProvider";
 import Table from "./Table";
 
 const App = () => (
-  <DataProvider endpoint="charcters" 
+  <DataProvider endpoint="characters" 
                 render={data => <Table data={data} />} />
 );
 const wrapper = document.getElementById("app");
