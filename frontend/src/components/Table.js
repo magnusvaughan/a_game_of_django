4ac0ce7ac10170252ce957b3c32780bde63d3e9e
mvaughan <magnus.vaughan@gmail.com>
2019-05-07 20:25:15 +0100

    Working pulling data from bulma

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -5,28 +5,25 @@ const Table = ({ data }) =>
   !data.length ? (
     <p>Nothing to show</p>
   ) : (
-
-    <div className="container">
-        <div className="col-md-12">
-            <h2 className="subtitle">
-                Showing <strong>{data.length} items</strong>
-            </h2>
-            <table className="table is-striped">
-                <thead>
-                <tr>
-                    {Object.entries(data[0]).map(el => <th key={key(el)}>{el[0]}</th>)}
-                </tr>
-                </thead>
-                <tbody>
-                {data.map(el => (
-                    <tr key={el.id}>
-                    {Object.entries(el).map(el => <td key={key(el)}>{el[1]}</td>)}
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
-    </div>
+      <div className="column">
+          <h2 className="subtitle">
+              Showing <strong>{data.length} items</strong>
+          </h2>
+          <table className="table is-striped">
+              <thead>
+              <tr>
+                  {Object.entries(data[0]).map(el => <th key={key(el)}>{el[0]}</th>)}
+              </tr>
+              </thead>
+              <tbody>
+              {data.map(el => (
+                  <tr key={el.id}>
+                  {Object.entries(el).map(el => <td key={key(el)}>{el[1]}</td>)}
+                  </tr>
+              ))}
+              </tbody>
+          </table>
+      </div>
   );
 Table.propTypes = {
   data: PropTypes.array.isRequired
